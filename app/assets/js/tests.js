4d9f19a51fec14c615f34769f663f2b8c4bc53b1
Roman Sologor <solohor.roman@gmail.com>
2018-10-23 21:34:17 +0300

    initial structure

---
+++ app/assets/js/tests.js
@@ -0,0 +1,93 @@
+// if (event.target.id === qustion.right_answer) {
+
+// }
+const tests = [
+    {   
+        id: 1,
+        questions: [
+            {   
+                id: 1,
+                right_answer: 1
+            },
+            {   
+                id: 1,
+                right_answer: 1
+            },
+            {   
+                id: 1,
+                right_answer: 1
+            },
+            {   
+                id: 1,
+                right_answer: 1
+            },
+            {   
+                id: 1,
+                right_answer: 1
+            },
+            {   
+                id: 1,
+                right_answer: 1
+            },
+        ]
+    },
+    {   
+        id: 2,
+        questions: [
+            {   
+                id: 1,
+                right_answer: 1
+            },
+            {   
+                id: 1,
+                right_answer: 1
+            },
+            {   
+                id: 1,
+                right_answer: 1
+            },
+            {   
+                id: 1,
+                right_answer: 1
+            },
+            {   
+                id: 1,
+                right_answer: 1
+            },
+            {   
+                id: 1,
+                right_answer: 1
+            },
+        ]
+    },
+    {   
+        id: 3,
+        questions: [
+            {   
+                id: 1,
+                right_answer: 1
+            },
+            {   
+                id: 1,
+                right_answer: 1
+            },
+            {   
+                id: 1,
+                right_answer: 1
+            },
+            {   
+                id: 1,
+                right_answer: 1
+            },
+            {   
+                id: 1,
+                right_answer: 1
+            },
+            {   
+                id: 1,
+                right_answer: 1
+            },
+        ]
+    },
+]
+
